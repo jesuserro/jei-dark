88cd6f8e7cfd9b427c8d581d4dc4eb5715c3d777
olagato <olagato@gmail.com>
2023-02-27 09:05:26 +0100

    chore: commit save point

---
+++ sample.ts
@@ -0,0 +1,36 @@
+import type { Book } from '~/models';
+
+export class Kindtoc {
+	books: Book[];
+
+	constructor(books: Book[]) {
+		this.books = books;
+		this.init();
+	}
+
+	init(): void {
+    // stuff
+	}
+
+
+  public getBooks(): Book[] {
+    return this.books;
+  }
+
+  /**
+   *
+   * @param asin string
+   * @returns Book[]
+   *
+   * B00UVRQDA8 Abandono
+   * B01LY1D0KZ Biblia
+   */
+  public getBookByAsin(asin: string):Book[] {
+    const book = this.books.find( book => book.asin === asin );
+    let mybooks = [];
+    if(book){
+      mybooks = [ book ];
+    }
+    return mybooks;
+  }
+}
